5d12af015b1aa8be2b0732695763a38eb3b2073b
= <=>
2018-02-22 09:13:46 -0700

    Added controllers and test dependencies.

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,4 +1,41 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class EnvController {
+
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddress;
+
+    public EnvController(
+            @Value("${PORT:NOT SET}") String port,
+            @Value("${MEMORY_LIMIT:NOT SET}") String memoryLimit,
+            @Value("${CF_INSTANCE_INDEX:NOT SET}") String cfInstanceIndex,
+            @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfInstanceAddress
+    ) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddress = cfInstanceAddress;
+    }
+
+    @RequestMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", this.port);
+        env.put("MEMORY_LIMIT", this.memoryLimit);
+        env.put("CF_INSTANCE_INDEX", this.cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", this.cfInstanceAddress);
+
+        return env;
+    }
 }
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,15 +1,21 @@
 
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
-    public class WelcomeController {
 
-        @RequestMapping("/")
-        public String sayHello() {
-            return "hello";
-        }
+@RestController
+public class WelcomeController {
+    private final String myMessage;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String myMessage) {
+        this.myMessage = myMessage;
     }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return myMessage;
+    }
+}
